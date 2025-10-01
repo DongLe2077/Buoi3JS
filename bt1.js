374e65a6fa28d21fee741c3467d6d313d6d0cf09
LeBaDong <badongle.2006@gmail.com>
2025-10-01 20:39:08 +0700

    hoan thanh bai tap

--- bt1.js
+++ bt1.js
@@ -1,8 +1,6 @@
 let giasp = prompt("Nhập giá sản phẩm: ");
 let soluong = prompt("Nhập số lượng sản phẩm: ");
-
 total = giasp * soluong;
-
 total -=total * 0.1;
 total += total * 0.05;
 
--- bt2.js
+++ bt2.js
@@ -1,3 +1,2 @@
 let number = parseInt(prompt("Nhập một số:"));
-
 console.log(number % 2 === 0 ? "Chẵn" : "Lẻ");
